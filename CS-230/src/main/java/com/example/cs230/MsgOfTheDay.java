4350bc4db9766aade4b57804fbec2f4b0675b643
Spooky78 <vic78dark@gmail.com>
2022-11-17 13:27:15 +0000

    Checkstyle checked.

--- CS-230/src/main/java/com/example/cs230/MsgOfTheDay.java
+++ CS-230/src/main/java/com/example/cs230/MsgOfTheDay.java
@@ -4,29 +4,35 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
 
+/**
+ * Responsible for message of the day.
+ * @author Rex
+ */
 public class MsgOfTheDay {
     public static void main(String[] args) {
         getRequest();
         shifting(inputS);
     }
 
+    private static final int STRING_LENGTH = 100;
+    private static final int ALPHABET_LENGTH = 26;
     private static String inputS;
     private static String outputS;
-    private static String cs230 = "CS-230";
-    private static String MsgOfTheDay;
+    private static final String CS230 = "CS-230";
+    private static String msgOfTheDay;
     // MsgOfTheDay is the stored output.
 
     public static void shifting(String str) {
         //get the number of characters in the final result. And put it to outputS(String).
-        outputS = Integer.toString(cs230.length() + str.length());
+        outputS = Integer.toString(CS230.length() + str.length());
 
-        String out[] = new String[100];
+        String[] out = new String[STRING_LENGTH];
         char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
                 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
         //put the string into a character array
-        char ch[] = new char[100];
+        char[] ch = new char[STRING_LENGTH];
         ch = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
             int characterVal = 0;
@@ -42,23 +48,21 @@ public class MsgOfTheDay {
             int shiftingVal = 0;
 
             // to prevent overflowing past 25
-            if (characterPla > 26) {
-                characterPla %= 26;
+            if (characterPla > ALPHABET_LENGTH) {
+                characterPla %= ALPHABET_LENGTH;
             }
-            
             //Check if the index of the character array is even or odd
             if (characterPla % 2 == 1) {
                 shiftingVal = characterVal - characterPla;
             } else {
                 shiftingVal = characterVal + characterPla;
             }
-
-            //encoding for example index 0, +1; index 1, -2; index 2, +3; 
+            //encoding for example index 0, +1; index 1, -2; index 2, +3;
             if (shiftingVal > letters.length) {
                 shiftingVal = shiftingVal - letters.length;
             } else if (shiftingVal < 0) {
                 shiftingVal += letters.length;
-            } else if (shiftingVal == 26) {
+            } else if (shiftingVal == ALPHABET_LENGTH) {
                 shiftingVal = 0;
             }
                 //change the value from integers to string
@@ -69,22 +73,23 @@ public class MsgOfTheDay {
                 outputS += out[i];
 
         }
-        outputS += cs230;
+        outputS += CS230;
         //outputS would be number of characters + encoded puzzle + CS-230
         sendSol();
     }
 
     public static void getRequest() {
         //get string from html to java
-        try{
+        try {
             URL url = new URL("http://cswebcat.swansea.ac.uk/puzzle");
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("GET");
-            String line ="";
-            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            String line = "";
+            InputStreamReader inputStreamReader =
+                new InputStreamReader(httpURLConnection.getInputStream());
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder response = new StringBuilder();
-            while ((line= bufferedReader.readLine())!=null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 response.append(line);
             }
             bufferedReader.close();
@@ -94,24 +99,26 @@ public class MsgOfTheDay {
             System.out.print("Error");
         }
     }
-    
+
+
     public static void sendSol() {
         //send the solution to the html URL and gets the message of the day string.
-        try{
+        try {
             String htmlLink = "http://cswebcat.swansea.ac.uk/message?solution=" + outputS;
             URL url = new URL(htmlLink);
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod("GET");
-            String line ="";
-            InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
+            String line = "";
+            InputStreamReader inputStreamReader =
+                new InputStreamReader(httpURLConnection.getInputStream());
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder response = new StringBuilder();
-            while ((line= bufferedReader.readLine())!=null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 response.append(line);
             }
             bufferedReader.close();
-            MsgOfTheDay = response.toString();
-            System.out.println(MsgOfTheDay); //current output method
+            msgOfTheDay = response.toString();
+            System.out.println(msgOfTheDay); //current output method
             //return MsgOfTheDay
 
         } catch (Exception e) {
