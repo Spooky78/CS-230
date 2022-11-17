b08c1d152887b1fda6622fc8b789f0376fdbdf3d
Spooky78 <vic78dark@gmail.com>
2022-11-17 19:14:02 +0000

    Initial Main Menu.

---
+++ CS-230/src/main/java/com/example/cs230/MainMenuButton.java
@@ -0,0 +1,121 @@
+package com.example.cs230;
+
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.text.Font;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Responsible for the button used on main menu.
+ */
+public class MainMenuButton extends Button {
+    private final String FONT_PATH;
+    {
+        try {
+            FONT_PATH = String.valueOf(new File(ClassLoader.getSystemResource(
+                "kenvector_future.ttf").toURI()));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private final URI BUTTON_PRESSED_PATH;
+    {
+        try {
+            BUTTON_PRESSED_PATH = new File(ClassLoader.getSystemResource(
+                "yellow_button_pressed.png").toURI()).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private final URI BUTTON_FREE_PATH;
+    {
+        try {
+            BUTTON_FREE_PATH = new File(ClassLoader.getSystemResource(
+                "yellow_button.png").toURI()).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private final String BUTTON_PRESSED_STYLE =
+        "-fx-background-color: transparent; -fx-background-image: url('"
+            + BUTTON_PRESSED_PATH + "');";
+    private final String BUTTON_FREE_STYLE =
+        "-fx-background-color: transparent; -fx-background-image: url('"
+            + BUTTON_FREE_PATH + "');";
+    private static final int PREF_WIDTH = 190;
+    private static final int PREF_HEIGHT = 45;
+    private static final int PRESSED_PREF_HEIGHT = 49;
+    private static final int PRESSED_HEIGHT_OFFSET = 4;
+    private static final int FONT_SIZE = 23;
+
+    /**
+     * Creates a main menu button.
+     * @param text the text that the button will display.
+     */
+    public MainMenuButton(String text) {
+        setText(text);
+        setButtonFont();
+        setPrefWidth(PREF_WIDTH);
+        setPrefHeight(PREF_HEIGHT);
+        setStyle(BUTTON_FREE_STYLE);
+        initializeButtonListener();
+    }
+
+    /**
+     * Sets the button text font & size.
+     */
+    private void setButtonFont() {
+        try {
+            setFont(Font.loadFont(new FileInputStream(FONT_PATH), FONT_SIZE));
+        } catch (FileNotFoundException e) {
+            setFont(Font.font("Verdana", FONT_SIZE));
+        }
+    }
+
+    /**
+     * Sets button pressed style.
+     */
+    private void setButtonPressedStyle() {
+        setStyle(BUTTON_PRESSED_STYLE);
+        setPrefHeight(PRESSED_PREF_HEIGHT);
+        setLayoutY(getLayoutY() + PRESSED_HEIGHT_OFFSET);
+    }
+
+    /**
+     * Sets button released style.
+     */
+    private void setButtonReleasedStyle() {
+        setStyle(BUTTON_FREE_STYLE);
+        setPrefHeight(PREF_HEIGHT);
+        setLayoutY(getLayoutY() - PRESSED_HEIGHT_OFFSET);
+    }
+
+    /**
+     * Initializes button listeners. Listens for mouse events for button to be
+     * pressed and released.
+     */
+    private void initializeButtonListener() {
+
+        setOnMousePressed(mouseEvent -> {
+            if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                setButtonPressedStyle();
+            }
+        });
+
+        setOnMouseReleased(mouseEvent -> {
+            if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                setButtonReleasedStyle();
+            }
+        });
+
+        setOnMouseEntered(mouseEvent -> setEffect(new DropShadow()));
+
+        setOnMouseExited(mouseEvent -> setEffect(null));
+    }
+}
---
+++ CS-230/src/main/java/com/example/cs230/ViewManager.java
@@ -0,0 +1,172 @@
+package com.example.cs230;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ * Responsible for the main menu window.
+ * @author Spooky78
+ */
+public class ViewManager {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private final BorderPane mainPane;
+    private final Scene mainScene;
+    private final Stage mainStage;
+    private static final String BACKGROUND_PATH = "treesBackground.png";
+    private static VBox buttonPane;
+    private MainMenuSubScene creditsSubScene;
+    private MainMenuSubScene helpSubScene;
+    private MainMenuSubScene scoreSubScene;
+    private MainMenuSubScene shipChooserSubScene;
+    private MainMenuSubScene sceneToHide;
+
+    /**
+     * Creates a main menu window.
+     */
+    public ViewManager() {
+        mainPane = new BorderPane();
+        mainScene = new Scene(mainPane, WIDTH, HEIGHT);
+        mainStage = new Stage();
+        mainStage.setScene(mainScene);
+        createSubScenes();
+        createBackground();
+        createLogo();
+        createButtons();
+
+    }
+
+    /**
+     * If sub scene is hidden then move it.
+     * @param subScene The sub scene to be moved.
+     */
+    private void showSubScene(MainMenuSubScene subScene) {
+        if (sceneToHide != null) {
+            sceneToHide.moveSubScene();
+        }
+
+        subScene.moveSubScene();
+        sceneToHide = subScene;
+    }
+
+    /**
+     * Creates the sub scenes for the main menu.
+     */
+    private void createSubScenes() {
+        creditsSubScene = new MainMenuSubScene();
+        mainPane.getChildren().add(creditsSubScene);
+
+        helpSubScene = new MainMenuSubScene();
+        mainPane.getChildren().add(helpSubScene);
+
+        scoreSubScene = new MainMenuSubScene();
+        mainPane.getChildren().add(scoreSubScene);
+
+        shipChooserSubScene = new MainMenuSubScene();
+        mainPane.getChildren().add(shipChooserSubScene);
+    }
+
+
+    /**
+     * Gets main stage.
+     * @return The main stage.
+     */
+    public Stage getMainStage() {
+        return mainStage;
+    }
+
+    /**
+     * Creates all main menu buttons.
+     */
+    private void createButtons() {
+        buttonPane = new VBox();
+        buttonPane.setSpacing(20);
+        buttonPane.setAlignment(Pos.CENTER_LEFT);
+        buttonPane.setPadding(new Insets(0,20,20,60));
+        mainPane.setLeft(buttonPane);
+        createStartButton();
+        createScoreButton();
+        createHelpButton();
+        createCreditsButton();
+        createExitButton();
+    }
+
+    /**
+     * Creates the play button.
+     */
+    private void createStartButton() {
+        MainMenuButton startButton = new MainMenuButton("PLAY");
+        buttonPane.getChildren().add(startButton);
+        startButton.setOnAction(actionEvent -> showSubScene(shipChooserSubScene));
+    }
+
+    /**
+     * Creates the score button.
+     */
+    private void createScoreButton() {
+        MainMenuButton scoreButton = new MainMenuButton("Scores");
+        buttonPane.getChildren().add(scoreButton);
+        scoreButton.setOnAction(actionEvent -> showSubScene(scoreSubScene));
+    }
+
+    /**
+     * Creates the help button.
+     */
+    private void createHelpButton() {
+        MainMenuButton helpButton = new MainMenuButton("Help");
+        buttonPane.getChildren().add(helpButton);
+        helpButton.setOnAction(actionEvent -> showSubScene(helpSubScene));
+    }
+
+    /**
+     * Creates the credits button.
+     */
+    private void createCreditsButton() {
+        MainMenuButton creditsButton = new MainMenuButton("Credits");
+        buttonPane.getChildren().add(creditsButton);
+        creditsButton.setOnAction(actionEvent -> showSubScene(creditsSubScene));
+    }
+
+    /**
+     * Creates the exit button, that exits the application.
+     */
+    private void createExitButton() {
+        MainMenuButton exitButton = new MainMenuButton("Exit");
+        buttonPane.getChildren().add(exitButton);
+        exitButton.setOnAction(actionEvent -> mainStage.close());
+    }
+
+    /**
+     * Sets the background to images.
+     */
+    private void createBackground() {
+        Image backgroundImage = new Image(BACKGROUND_PATH, WIDTH, HEIGHT, false, false);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(100, 100,
+            true, true, true, true));
+        mainPane.setBackground(new Background(background));
+    }
+
+    /**
+     * Creates the game logo.
+     */
+    private void createLogo() {
+
+        ImageView logo = new ImageView("logo.png");
+
+        logo.setOnMouseEntered(mouseEvent -> logo.setEffect(new DropShadow()));
+        logo.setOnMouseExited(mouseEvent -> logo.setEffect(null));
+
+        VBox logoPane = new VBox();
+        logoPane.getChildren().add(logo);
+        logoPane.setAlignment(Pos.TOP_CENTER);
+        logoPane.setPadding(new Insets(30,0,0,0));
+        mainPane.setTop(logoPane);
+    }
+}
