e42b619c55c5138fb52038454b08c6be194412ea
geros22 <chrysis.pitsillides@gmail.com>
2022-12-06 10:32:01 +0000

    Started bomb collision + some code reformatting , have some questions

--- CS-230/src/main/java/com/example/cs230/Bomb.java
+++ CS-230/src/main/java/com/example/cs230/Bomb.java
@@ -3,18 +3,22 @@ package com.example.cs230;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+
 import java.util.Objects;
 
 public class Bomb {
     private static final String BOMB_PATH = "/Items/bomb.png";
+    private boolean isPicked = false;
     private ImageView bomb = new ImageView();
-    private StackPane bombPane = new StackPane();
-    private Board currentBoard;
+    private final StackPane bombPane = new StackPane();
+    private final Board currentBoard;
+    private int[] bombPosition;
 
     public Bomb(Board currentBoard, int[] position) {
         this.currentBoard = currentBoard;
         createItem(position);
     }
+
     protected void createItem(int[] position) {
         Image bombImage = new Image(
                 Objects.requireNonNull(getClass().getResourceAsStream(BOMB_PATH)));
@@ -25,7 +29,14 @@ public class Bomb {
         int tileSize = currentBoard.getTileSize();
         bombPane.setLayoutX((position[0] * tileSize - tileSize / 2));
         bombPane.setLayoutY((position[1] * tileSize - tileSize / 2));
+        bombPosition = currentBoard.getDoorCoords();
     }
 
-    public StackPane getBombPane() { return bombPane; }
-}
+    public boolean isCollisionPlayer(int[] playerCoords) {
+        return playerCoords[0] + 1 == bombPosition[0] && playerCoords[1] + 1 == bombPosition[1];
+    }
+
+        public StackPane getBombPane() {
+            return bombPane;
+        }
+    }
